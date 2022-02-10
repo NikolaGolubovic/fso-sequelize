117c63d3495ebbd4b670ed7049146673a32e731e
NikolaGolubovic <nikolagolubovic985@gmail.com>
2022-02-10 10:49:55 +0100

    tasks 13.5-13.7

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
 require("dotenv").config();
-const Blog = require("./models/Blog");
 const express = require("express");
+
+const middleware = require("./utils/middleware");
+const { connectToDatabase } = require("./utils/db");
+const blogController = require("./controllers/blogs");
+const { PORT } = require("./utils/config");
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/blogs", async (req, res) => {
-  try {
-    const blogs = await Blog.findAll();
-    res.json(blogs);
-  } catch (error) {
-    console.log(error);
-  }
-});
+app.use("/api/blogs", blogController);
 
-app.post("/api/blogs", async (req, res) => {
-  try {
-    const { author, url, title } = req.body;
-    const blog = await Blog.create({ author, url, title });
-    return res.status(200).json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log("Server running on port 3000");
+  });
+};
 
-app.delete("/api/blogs/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    await Blog.destroy({ where: { id } });
-    res.status(200).send("Record is successfuly deleted");
-  } catch (error) {
-    console.log("/api/blogs/:id delete", error);
-  }
-});
+app.use(middleware.errorHandler);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port 3000");
-});
+start();
