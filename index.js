3550d2b0e08c58f8218d9a78b228f200a01cafbb
NikolaGolubovic <nikolagolubovic985@gmail.com>
2022-02-10 09:33:00 +0100

    tasks 13.1 - 13.4

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.delete("/api/blogs/:id", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });
