0435f1a2e1d2d0d82c202ffbbdd6507c41484c73
NikolaGolubovic <nikolagolubovic985@gmail.com>
2022-02-10 15:33:35 +0100

    13.8-13.11 exercise section completed

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,27 @@
 const router = require("express").Router();
-const Blog = require("../models/Blog");
+const { Blog } = require("../models");
+const { User } = require("../models");
+const { tokenExtractor } = require("../util/middleware");
 
 router.get("/", async (req, res, next) => {
   try {
-    const blogs = await Blog.findAll();
+    const blogs = await Blog.findAll({
+      include: {
+        model: User,
+        attributes: ["username"],
+      },
+    });
     res.json(blogs);
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/", tokenExtractor, async (req, res, next) => {
   try {
     const { author, url, title } = req.body;
-    const blog = await Blog.create({ author, url, title });
+    const user = await User.findByPk(req.decodedToken.id);
+    const blog = await Blog.create({ author, url, title, userId: user.id });
     return res.status(200).json(blog);
   } catch (error) {
     return res.status(400).json({ error });
@@ -33,10 +41,13 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 
-router.put("/:id", async (req, res, next) => {
+router.put("/:id", tokenExtractor, async (req, res, next) => {
   try {
     const id = req.params.id;
     const blog = await Blog.findByPk(id);
+    if (blog.userId !== req.decodedToken.id) {
+      throw new Error("Your don't have permision for the action");
+    }
     if (blog === null) {
       throw new Error("Blog is not found");
     }
---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../util/db");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allownull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        is: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
